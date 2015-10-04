c0e0505eb64e942f97097b7f2dbab4dc0942ac35
catperez81 <cat@healthsherpa.com>
2015-10-04 11:49:10 -0700

    run script test

---
+++ js/application.js
@@ -0,0 +1,13 @@
+
+// $(document).ready(function() {
+//   alert('Oh, hi there, world');
+// });
+
+
+var appNumber = 1;
+while(appNumber <= 50) {
+	console.log(appNumber);
+	appNumber++;
+}
+// var appNumber1 = appNumber+=1;
+// console.log(appNumber1);
