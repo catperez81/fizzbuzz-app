73a4f91510d8d1ca4b8e762b925049e32e45badc
catperez81 <cat@healthsherpa.com>
2015-10-07 18:29:50 -0700

    update javascript

--- js/application.js
+++ js/application.js
@@ -5,9 +5,25 @@
 
 
 var appNumber = 1;
-while(appNumber <= 50) {
+while(appNumber <= 100) {
+	// $('#number').append(appNumber + '<br>');
+	$('#number').append("test");
+	if(appNumber % 3 == 0 && appNumber % 5 == 0) {
+		console.log("fizz-buzz");
+	}
+	else if(appNumber % 3 == 0) {
+		console.log("fizz");
+	}
+	else if(appNumber % 5 == 0) {
+		console.log("buzz");
+	}
+	else {
 	console.log(appNumber);
+	}
 	appNumber++;
 }
+
+
+
 // var appNumber1 = appNumber+=1;
 // console.log(appNumber1);
