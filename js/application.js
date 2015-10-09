0c7d635eddec45b9dde3d4894d5d09616abef295
catperez81 <cat@healthsherpa.com>
2015-10-08 19:12:44 -0700

    update append in java

--- js/application.js
+++ js/application.js
@@ -1,28 +1,29 @@
 
-// $(document).ready(function() {
-//   alert('Oh, hi there, world');
-// });
-
+$(document).ready(function() {
 
 var appNumber = 1;
 while(appNumber <= 100) {
 	// $('#number').append(appNumber + '<br>');
-	$('#number').append("test");
 	if(appNumber % 3 == 0 && appNumber % 5 == 0) {
 		console.log("fizz-buzz");
+		$('#number').append('<li>fizz-buzz</li>');
 	}
 	else if(appNumber % 3 == 0) {
 		console.log("fizz");
+		$('#number').append('<li>fizz</li>');
 	}
 	else if(appNumber % 5 == 0) {
 		console.log("buzz");
+		$('#number').append('<li>buzz</li>');
 	}
 	else {
 	console.log(appNumber);
+	$('#number').append('<li>' + appNumber + '</li>');
 	}
 	appNumber++;
 }
 
+});
 
 
 // var appNumber1 = appNumber+=1;
